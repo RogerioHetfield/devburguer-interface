5a5b6596b3e20b69bb7eda47b17137d5b6dfcdd8
RogerioHetfield <rgfm.95@gmail.com>
2026-03-02 16:16:26 -0300

    devburguer-interface

---
+++ src/components/Stripe/CheckoutForm/index.jsx
@@ -0,0 +1,10 @@
+import { useLocation } from "react-router-dom";
+
+export function Checkout() {
+    const location = useLocation();
+
+    return (
+        <div>
+            <h1>Form</h1>
+        </div>
+    )};
---
+++ src/containers/Checkout/index.jsx
@@ -0,0 +1,11 @@
+import { useLocation } from "react-router-dom";
+
+export function Checkout() {
+    const location = useLocation();
+
+    return (
+        <Elements>
+            <h1>Checkout</h1>
+        </Elements>
+    );
+}
